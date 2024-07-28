9c6d667a75aabd9615be5a641fa5cb59c97c1d3f
Dhruv <dhruvofficial0108@gmail.com>
2024-07-29 02:31:59 +0530

    login and sign-up done

--- login.php
+++ login.php
@@ -1,29 +1,22 @@
 <?php
-$user=0;
-$s=0;
+$login=0;
+$invalid=0;
 
 if($_SERVER['REQUEST_METHOD']=='POST'){
     include 'connect.php';
     $username=$_POST['username'];
     $password=$_POST['password'];
     
-     $sql="select * from registration where username='$username'";
+     $sql="select * from registration where username='$username' and password='$password'";
      $result=mysqli_query($con,$sql);
      if($result){
         $num=mysqli_num_rows($result);
         if($num>0){
-          #  echo "Username already exists";
-          $user=1;
+          #  echo "Login successfull";
+          $login=1;
           
         }else{
-            $sql="insert into registration(username,password) values('$username','$password')";
-            $result=mysqli_query($con,$sql);
-            if($result){
-              $s=1;
-               # header('Location:signup.php');
-            }else{
-                die(mysqli_error($result));
-            }
+            $invalid=1;
         }
      }
     }
@@ -50,34 +43,26 @@ if($_SERVER['REQUEST_METHOD']=='POST'){
 <!-- Include Bootstrap JS -->
 <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js"></script>
 
-    <title>Sign-up</title>
+    <title>Login Page</title>
 </head>
 
 <body>
-    <?php
-if($user){
-echo '<div class="alert alert-danger alert-dismissible fade show" role="alert">
-  <strong>Sorry!</strong>The username already exists!
-  <button type="button" class="close" data-dismiss="alert" aria-label="Close">
-    <span aria-hidden="true">&times;</span>
-  </button>
-</div>';
-}?>
-<?php
-
-if($s){
-    echo '<div class="alert alert-success alert-dismissible fade show" role="alert">
-      <strong>Congratulation!</strong>Sign-up Completed!
+<?php if($login){echo '<div class="alert alert-success alert-dismissible fade show" role="alert">
+      <strong>Congratulation!</strong>Login Successfull !
       <button type="button" class="close" data-dismiss="alert" aria-label="Close">
         <span aria-hidden="true">&times;</span>
       </button>
-    </div>';
-    }
+    </div>';} ?>
 
-
-?>
-
-    <h1 class="text-center my-5">Sign-Up</h1>
+    <?php if($invalid){
+    echo '<div class="alert alert-danger alert-dismissible fade show" role="alert">
+      <strong>Sorry!</strong>Invalid Credentials!
+      <button type="button" class="close" data-dismiss="alert" aria-label="Close">
+        <span aria-hidden="true">&times;</span>
+      </button>
+    </div>'; }
+    ?>
+    <h1 class="text-center my-5">Login to my page</h1>
     <div class="container my-5">
         <form method="POST" action="signup.php">
             <div class="mb-3">
@@ -88,7 +73,7 @@ if($s){
                 <label class="form-label">Password</label>
                 <input type="password" class="form-control" name="password" placeholder="Enter your password">
             </div>
-            <button type="submit" class="btn btn-primary w-100">Signup</button>
+            <button type="submit" class="btn btn-primary w-100">Login</button>
         </form>
     </div>
    
